27dd83e71270d6d27cbacd97019f24b0184aa5e1
Henry Grech-Cini <henry.grechcini@gmail.com>
2016-03-25 17:57:50 +0000

    Basic client and service

---
+++ client.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henrygc
+ * Date: 25/03/16
+ * Time: 17:55
+ */
+
+/*
+*  Hello World client
+*  Connects REQ socket to tcp://localhost:5555
+*  Sends "Hello" to server, expects "World" back
+* @author Ian Barber <ian(dot)barber(at)gmail(dot)com>
+*/
+
+$context = new ZMQContext();
+
+//  Socket to talk to server
+echo "Connecting to hello world server…\n";
+$requester = new ZMQSocket($context, ZMQ::SOCKET_REQ);
+$requester->connect("tcp://localhost:5555");
+
+for ($request_nbr = 0; $request_nbr != 10; $request_nbr++) {
+    printf ("Sending request %d…\n", $request_nbr);
+    $requester->send("Hello");
+
+    $reply = $requester->recv();
+    printf ("Received reply %d: [%s]\n", $request_nbr, $reply);
+}
