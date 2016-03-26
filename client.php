8f26fd52f8daa783dd8e52f9e491646dd7f85e39
Henry Grech-Cini <henry.grechcini@gmail.com>
2016-03-26 14:10:49 +0000

    Changed to ZMQ_PUB as opposed to ZMQ_REQ since ZMQ_PUB will DROP on an action mute state

--- client.php
+++ client.php
@@ -17,12 +17,12 @@ $context = new ZMQContext();
 
 //  Socket to talk to server
 echo "Connecting to hello world server…\n";
-$requester = new ZMQSocket($context, ZMQ::SOCKET_REQ);
+$requester = new ZMQSocket($context, ZMQ::SOCKET_PUB);
 $requester->connect("tcp://192.168.1.45:5555");
 
 for ($request_nbr = 0; $request_nbr != 10; $request_nbr++) {
     printf ("Sending request %d…\n", $request_nbr);
-    $requester->send("Hello", ZMQ::MODE_DONTWAIT || ZMQ::MODE_NOBLOCK);
+    $requester->send("Hello", ZMQ::MODE_DONTWAIT);
 
     $reply = $requester->recv();
     printf ("Received reply %d: [%s]\n", $request_nbr, $reply);
