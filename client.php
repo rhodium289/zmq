4cd39cd69e2b3610a5b307d1e96a456201265931
Henry Grech-Cini <henry.grechcini@gmail.com>
2016-03-26 13:32:42 +0000

    Added DONTWAIT

--- client.php
+++ client.php
@@ -18,11 +18,11 @@ $context = new ZMQContext();
 //  Socket to talk to server
 echo "Connecting to hello world server…\n";
 $requester = new ZMQSocket($context, ZMQ::SOCKET_REQ);
-$requester->connect("tcp://localhost:5555");
+$requester->connect("tcp://192.168.1.45:5555");
 
 for ($request_nbr = 0; $request_nbr != 10; $request_nbr++) {
     printf ("Sending request %d…\n", $request_nbr);
-    $requester->send("Hello");
+    $requester->send("Hello", ZMQ::MODE_DONTWAIT);
 
     $reply = $requester->recv();
     printf ("Received reply %d: [%s]\n", $request_nbr, $reply);
