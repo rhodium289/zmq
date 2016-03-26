0ab07e5229cfaab75bc66aaac428e29651bca8c5
Henry Grech-Cini <henry.grechcini@gmail.com>
2016-03-26 14:04:16 +0000

    Basic client and service

--- client.php
+++ client.php
@@ -22,7 +22,7 @@ $requester->connect("tcp://192.168.1.45:5555");
 
 for ($request_nbr = 0; $request_nbr != 10; $request_nbr++) {
     printf ("Sending request %d…\n", $request_nbr);
-    $requester->send("Hello", ZMQ::MODE_DONTWAIT);
+    $requester->send("Hello", ZMQ::MODE_DONTWAIT || ZMQ::MODE_NOBLOCK);
 
     $reply = $requester->recv();
     printf ("Received reply %d: [%s]\n", $request_nbr, $reply);
